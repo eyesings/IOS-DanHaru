469948ebc1b4513fdf4b7afd31bfa730c93b362e
kang_hyeonjoong <kang_hyeonjoong@radcns.com>
2021-10-25 14:32:36 +0900

    강현중 1. SnapKit 라이브러리 추가 2. Lottie 이미지 추가 3. 메인 페이지 UI 작성중

--- DanHaruApp/DanHaruProject/ViewController/Main/MainViewController.swift
+++ DanHaruApp/DanHaruProject/ViewController/Main/MainViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Lottie
+
 
 class MainViewController: UIViewController {
 
+    var dateLabel = UILabel()
+    
+    let calendarShowBtn = AnimationView(name: "3442-scroll")
+    
+    var addButton = UIButton()
+    
+    var formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
+    func setUI() {
+        
+        self.view.addSubview(dateLabel)
+        formatter.dateFormat = "yyyy-MM-dd"
+        var current_date_string = formatter.string(from: Date())
+        dateLabel.text = current_date_string
+        
+        
+    }
+    
 
 }
 
