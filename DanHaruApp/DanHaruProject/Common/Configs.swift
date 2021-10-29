fca5f5a7bff006554770b0c52d41730a83a87a35
kang_hyeonjoong <kang_hyeonjoong@radcns.com>
2021-10-29 14:37:13 +0900

    1. 푸시 확인

--- DanHaruApp/DanHaruProject/Common/Configs.swift
+++ DanHaruApp/DanHaruProject/Common/Configs.swift
@@ -20,3 +20,44 @@ enum InputType: Int {
     case pw = 103
     case done
 }
+
+struct Configs {
+    
+    static var formatter = DateFormatter()
+    
+    static var mainBackgroundColor = UIColor(red: 255/255, green: 252/255, blue: 252/255, alpha: 1.0)
+    
+    // 좀더 고민
+    enum todoListColor {
+        case tiffanyBlue
+        case mint
+        case yellow
+        case hotPink
+    }
+    
+    static var todoListCellBackGroundColor: [UIColor] = [
+        UIColor(red: 160/255, green: 231/255, blue: 229/255, alpha: 1.0),
+        UIColor(red: 180/255, green: 248/255, blue: 200/255, alpha: 1.0),
+        UIColor(red: 251/255, green: 231/255, blue: 198/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 174/255, blue: 188/255, alpha: 1.0)
+    ]
+    
+}
+
+
+extension Configs.todoListColor {
+    var value: UIColor {
+        get {
+            switch self {
+            case .tiffanyBlue:
+                return UIColor(red: 160/255, green: 231/255, blue: 229/255, alpha: 1.0)
+            case .mint:
+                return UIColor(red: 180/255, green: 248/255, blue: 200/255, alpha: 1.0)
+            case .yellow:
+                return UIColor(red: 251/255, green: 231/255, blue: 198/255, alpha: 1.0)
+            case .hotPink:
+                return UIColor(red: 255/255, green: 174/255, blue: 188/255, alpha: 1.0)
+            }
+        }
+    }
+}
